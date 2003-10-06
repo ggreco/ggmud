d7c50bf68130dfe5dc0ac1bf862d0cb2b784d90c
ggreco <ggreco@35a8c427-5c34-0410-aaab-be164168d6bc>
2003-10-06 13:50:00 +0000

    - Added highlight configuration panel. - Improved highlight code.

--- trunk/ggmud/highlights.c
+++ trunk/ggmud/highlights.c
@@ -1,14 +1,12 @@
 #include "ggmud.h"
+#include "ansi.h"
+
+extern struct listnode *common_highs;
 
 GtkWidget *menu_Tools_Highlight;
 
 #define HIGH_FILE "highlight"
-
-void
-highlights_window(GtkWidget *w, gpointer data)
-{
-    popup_window("HIGHLIGHTS settings: TODO");
-}
+#define ALIAS_LEN 128
 
 static void
 save_highlights (GtkWidget *button, gpointer data) {
@@ -18,26 +16,13 @@ save_highlights (GtkWidget *button, gpointer data) {
     gint  row = 0;
 
     if (fp = fileopen (HIGH_FILE, "w")) {
-    	while ( !done && (GtkCList*) data) {
-            if ( !gtk_clist_get_text ((GtkCList*) data, row, 0, &alias)
-                || !gtk_clist_get_text ((GtkCList*) data, row, 1, &replace) )
-                break;
-            
-            if (!alias[0]) {
-                done = TRUE;
-                break;
-            }
-            fprintf (fp, "%s %s\n", alias, replace);
-            row++;
+        struct listnode *list = mud->activesession ? mud->activesession->highs : common_highs;
+        
+        while ( list = list->next ) {
+            fprintf(fp, "#high {%s} {%s}\n", list->right, list->left);
         }
         fclose (fp);
     }    
-}
-
-static void
-add_highlight(int type, char *string)
-{
-    // TODO
 }
 
 void
@@ -51,3 +36,398 @@ load_highlights()
     }
 }
 
+static int high_selected_row = -1;
+static GtkWidget *high_window, *bgwidget, *fgwidget, *textalias;
+
+
+typedef struct {
+    char *name;
+    char *tt_name;
+    GdkColor *color;
+}
+color_options;
+
+static color_options bg_options[] =
+{
+    {"Black", "b black", &color_black},
+    {"Red", "b red", &color_red},
+    {"Green", "b green", &color_green},
+    {"Brown", "b brown", &color_yellow},
+    {"Blue", "b blue", &color_blue},
+    {"Magenta", "b magenta", &color_magenta},
+    {"Cyan", "b cyan", &color_cyan},
+    {"Grey", "b grey", &color_white},
+    {"Charcoal", "b charcoal", &color_lightblack},
+    {"Light red", "b light red", &color_lightred},
+    {"Light green", "b light green", &color_lightgreen},
+    {"Yellow", "b yellow", &color_lightyellow},
+    {"Light blue", "b light blue", &color_lightblue},
+    {"Light magenta", "b light magenta", &color_lightmagenta},
+    {"Light cyan", "b light cyan", &color_lightcyan},
+    {"White", "b white", &color_lightwhite},
+    {"Bold", "bold", NULL},
+    {"Italic", "italic", NULL},
+    {"Reverse", "reverse", NULL},
+    {NULL, NULL, NULL}
+};
+
+static color_options fg_options[] =
+{
+    {"Black", "black", &color_black},
+    {"Red", "red", &color_red},
+    {"Green", "green", &color_green},
+    {"Brown", "brown", &color_yellow},
+    {"Blue", "blue", &color_blue},
+    {"Magenta", "magenta", &color_magenta},
+    {"Cyan", "cyan", &color_cyan},
+    {"Grey", "grey", &color_white},
+    {"Charcoal", "charcoal", &color_lightblack},
+    {"Light red", "light red", &color_lightred},
+    {"Light green", "light green", &color_lightgreen},
+    {"Yellow", "yellow", &color_lightyellow},
+    {"Light blue", "light blue", &color_lightblue},
+    {"Light magenta", "light magenta", &color_lightmagenta},
+    {"Light cyan", "light cyan", &color_lightcyan},
+    {"White", "white", &color_lightwhite},
+    {NULL, NULL, NULL}
+};
+
+int get_optionmenu_active(GtkWidget *t)
+{
+    GtkWidget *k,*active_item;
+    
+    if (!(k = gtk_option_menu_get_menu(GTK_OPTION_MENU(t)))) {
+        return -1;
+    }
+        
+    if (!(active_item = gtk_menu_get_active (GTK_MENU (k)))) {
+        return -1;
+    }
+
+    return g_list_index (
+                GTK_MENU_SHELL(k)->children,
+                active_item);
+}
+
+static int get_options(char *text, color_options *opts)
+{
+    int i = 0;
+    
+    while(opts[i].name) {
+        if(!strcmp(text, opts[i].name))
+            return i;
+        
+        i++;
+    }
+    return -1;
+}
+
+static int get_options_tt(char *text, color_options *opts)
+{
+    int i = 0;
+    
+    while(opts[i].tt_name) {
+        if(!strcmp(text, opts[i].tt_name))
+            return i;
+        
+        i++;
+    }
+    return -1;
+}
+
+static void high_selection_made (GtkWidget *clist, gint row, gint column,
+                           GdkEventButton *event, gpointer data)
+{
+    gchar *text;
+    int i;
+    
+    high_selected_row    = row;
+
+    if ( (GtkCList*) data )
+    {
+        gtk_clist_get_text ((GtkCList*) data, row, 0, &text);
+        gtk_entry_set_text (GTK_ENTRY (textalias), text);
+        
+        gtk_clist_get_text ((GtkCList*) data, row, 1, &text);
+        i = get_options(text, bg_options);
+        if(i >= 0)
+            gtk_option_menu_set_history(GTK_OPTION_MENU(bgwidget), i);
+        
+        gtk_clist_get_text ((GtkCList*) data, row, 2, &text);
+        i = get_options(text, fg_options);
+        if(i >= 0)
+            gtk_option_menu_set_history(GTK_OPTION_MENU(fgwidget), i);
+    }
+    
+    return;
+}
+
+
+static void
+add_highlight(char *string, color_options *bg, color_options *fg)
+{
+    char buffer[200];
+
+    sprintf(buffer, "#high {%s, %s} {%s}",
+            bg->tt_name, fg->tt_name, string);
+            
+    parse_input(buffer, NULL);
+}
+
+static void high_button_add (GtkWidget *button, gpointer data)
+{
+    gchar *text[3];
+    gint   i, j;
+
+    text[0]   = gtk_entry_get_text (GTK_ENTRY (textalias  ));
+
+    i = get_optionmenu_active(bgwidget);
+    j = get_optionmenu_active(fgwidget);
+    
+    text[1] = bg_options[i].name;
+    text[2] = fg_options[j].name;
+    
+    if ( text[0][0] == '\0'  )
+    {
+        popup_window ("Please insert some text first.");
+        return;
+    }
+
+    if ( strlen (text[0]) > ALIAS_LEN)
+    {
+        popup_window ("String too big.");
+        return;
+    }
+    
+    gtk_clist_append ((GtkCList *) data, text);
+    add_highlight (text[0], &bg_options[i], &fg_options[j]);
+}
+
+static void high_button_delete (GtkWidget *button, gpointer data) {
+    gchar *word;
+    
+    if ( high_selected_row == -1 ) {
+        popup_window ("No selection made.");
+    }
+    else {
+        char buffer[ALIAS_LEN + 20];
+        
+        gtk_clist_get_text ((GtkCList*) data, high_selected_row, 0, &word);
+        sprintf(buffer, "#unhigh {%s}", word);
+        gtk_clist_remove ((GtkCList*) data, high_selected_row);
+        high_selected_row = -1;
+
+        parse_input(buffer, mud->activesession);
+    }
+
+}
+
+static void  insert_highlights  (GtkWidget *clist)
+{
+    gchar *text[3];
+    struct listnode *list = mud->activesession ? mud->activesession->highs : common_highs;
+    int i, j;
+    char buffer[200], *c, *d;
+    
+    while ( list = list->next ) {
+        text[0] = list->left;
+        
+        strcpy(buffer, list->right);
+
+        c = buffer;
+
+        i = j = - 1;
+        
+        while (*c != ',' && *c > 0)
+            c++;
+
+        if( *c == ',') {
+            char *e;
+
+            *c = 0;
+            c++;
+
+            while( *c == ' ' || *c == '\t')
+                c++;
+
+            e = c;
+            
+            while(*c != ',' && *c != 0)
+                c++;
+
+            *c = 0;
+            
+            if( (i = get_options_tt(e, bg_options)) < 0)
+                j = get_options_tt(e, fg_options);
+        }
+        
+        if( (i = get_options_tt(buffer, bg_options)) < 0)
+            j = get_options_tt(buffer, fg_options);
+        
+
+        if(i >= 0)
+            text[1] = bg_options[i].name;
+        else
+            text[1] = "None";
+        
+        if(j >= 0)
+            text[2] = fg_options[j].name;
+        else
+            text[2] = "None";
+        
+        gtk_clist_prepend (GTK_CLIST (clist), text);
+    }
+}
+
+static void append_options(GtkWidget *w, color_options *options)
+{
+    int i = 0;
+    GtkWidget *t;
+    GtkWidget *m = gtk_menu_new();
+    gtk_widget_show(m);
+    
+    while(options[i].name) {
+        t = gtk_menu_item_new_with_label (options[i].name);
+        gtk_widget_show(t);
+        gtk_menu_append(GTK_MENU(m), t);
+        i++;
+    }
+
+    gtk_option_menu_set_menu(GTK_OPTION_MENU(w), m);
+}
+
+void highlights_window (GtkWidget *widget, gpointer data)
+{
+    GtkWidget *vbox;
+    GtkWidget *hbox;
+    GtkWidget *hbox2;
+    GtkWidget *hbox3;
+    GtkWidget *clist;
+    GtkWidget *button_add;
+    GtkWidget *button_quit;
+    GtkWidget *button_delete;
+    GtkWidget *button_save;
+    GtkWidget *label;
+    GtkWidget *separator;
+    GtkTooltips *tooltip;
+    GtkWidget *scrolled_window;
+
+    gchar     *titles[3] = { "Highlight", "Background/Effect", "Foreground" };
+
+    tooltip = gtk_tooltips_new ();
+    gtk_tooltips_set_colors (tooltip, &color_lightyellow, &color_black);
+
+
+    high_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title (GTK_WINDOW (high_window), "Highlights");
+    gtk_signal_connect (GTK_OBJECT (high_window), "destroy",
+                               GTK_SIGNAL_FUNC(close_window), high_window );
+    gtk_widget_set_usize (high_window, 550, 320);			       
+    vbox = gtk_vbox_new (FALSE, 5);
+    gtk_container_set_border_width (GTK_CONTAINER (vbox), 0);
+    gtk_container_add (GTK_CONTAINER (high_window), vbox);
+    gtk_widget_show (vbox);
+
+    /* create a new scrolled window. */
+    scrolled_window = gtk_scrolled_window_new (NULL, NULL);
+    gtk_container_set_border_width (GTK_CONTAINER (scrolled_window), 0);
+    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window),
+                                    GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    gtk_box_pack_start (GTK_BOX (vbox), scrolled_window, TRUE, TRUE, 0);
+    gtk_widget_show (scrolled_window);
+
+    clist = gtk_clist_new_with_titles (3, titles);
+    gtk_signal_connect_object (GTK_OBJECT (clist), "select_row",
+                               GTK_SIGNAL_FUNC (high_selection_made),
+                               (gpointer) clist);
+    gtk_clist_column_titles_passive (GTK_CLIST (clist));
+    gtk_clist_set_shadow_type (GTK_CLIST (clist), GTK_SHADOW_IN);
+    gtk_clist_set_column_width (GTK_CLIST (clist), 0, 250);
+    gtk_clist_set_column_width (GTK_CLIST (clist), 1, 100);
+    gtk_clist_set_column_width (GTK_CLIST (clist), 2, 100);
+//    gtk_clist_set_column_justification (GTK_CLIST (clist), 0, GTK_JUSTIFY_LEFT);
+//    gtk_clist_set_column_justification (GTK_CLIST (clist), 1, GTK_JUSTIFY_LEFT);
+
+    gtk_clist_column_titles_show (GTK_CLIST (clist));
+
+    gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW (scrolled_window), clist);
+
+    gtk_widget_show (clist);
+
+    hbox3 = gtk_table_new(3, 2, FALSE);
+
+    gtk_box_pack_start (GTK_BOX (vbox), hbox3, FALSE, FALSE, 0);
+    gtk_widget_show (hbox3);
+
+    
+    label = gtk_label_new ("Text");
+    gtk_table_attach(GTK_TABLE(hbox3), label, 0, 1, 0, 1,
+                        GTK_FILL|GTK_EXPAND, /*GTK_FILL*/ 0L, 2, 2);
+    gtk_widget_show (label);
+    
+    label = gtk_label_new ("Back/Effect");
+    gtk_table_attach(GTK_TABLE(hbox3), label, 1, 2, 0, 1,
+                        0L, /*GTK_FILL*/ 0L, 2, 2);
+    gtk_widget_show (label);
+
+    label = gtk_label_new ("Foreground");
+    gtk_table_attach(GTK_TABLE(hbox3), label, 2, 3, 0, 1,
+                        0L, /*GTK_FILL*/ 0L, 2, 2);
+    gtk_widget_show (label);
+
+    textalias   = gtk_entry_new ();
+    gtk_table_attach(GTK_TABLE(hbox3), textalias, 0, 1, 1, 2,
+                         GTK_FILL|GTK_EXPAND,  0L, 2, 2);
+    gtk_widget_show (textalias  );
+
+
+    bgwidget = gtk_option_menu_new();
+    append_options(bgwidget, bg_options);
+    gtk_table_attach(GTK_TABLE(hbox3), bgwidget, 1, 2, 1, 2,
+                            0L, /*GTK_FILL*/ 0L, 2, 2);
+    gtk_widget_show (bgwidget);
+
+    fgwidget = gtk_option_menu_new();
+    append_options(fgwidget, fg_options);
+    gtk_table_attach(GTK_TABLE(hbox3), fgwidget, 2, 3, 1, 2,
+                            0L, /*GTK_FILL*/ 0L, 2, 2);
+    gtk_widget_show (fgwidget);
+    
+    separator = gtk_hseparator_new ();
+    gtk_box_pack_start (GTK_BOX (vbox), separator, FALSE, TRUE, 5);
+    gtk_widget_show (separator);
+
+    hbox = gtk_hbox_new (FALSE, 0);
+    gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 10);
+    gtk_widget_show (hbox);
+
+    button_add    = gtk_button_new_with_label ("  add   ");
+    button_quit   = gtk_button_new_with_label (" close  ");
+    button_delete = gtk_button_new_with_label (" delete ");
+    button_save   = gtk_button_new_with_label ("  save  ");
+    gtk_signal_connect (GTK_OBJECT (button_add), "clicked",
+                               GTK_SIGNAL_FUNC (high_button_add),
+                               (gpointer) clist);
+    gtk_signal_connect (GTK_OBJECT (button_delete), "clicked",
+                               GTK_SIGNAL_FUNC (high_button_delete),
+                               (gpointer) clist);
+    gtk_signal_connect (GTK_OBJECT (button_save), "clicked",
+                               GTK_SIGNAL_FUNC (save_highlights),
+                               (gpointer) clist);
+    gtk_signal_connect (GTK_OBJECT (button_quit), "clicked",
+                               GTK_SIGNAL_FUNC (close_window), high_window);
+
+    gtk_box_pack_start (GTK_BOX (hbox), button_add,    TRUE, TRUE, 15);
+    gtk_box_pack_start (GTK_BOX (hbox), button_delete, TRUE, TRUE, 15);
+    gtk_box_pack_start (GTK_BOX (hbox), button_save,   TRUE, TRUE, 15);
+    gtk_box_pack_start (GTK_BOX (hbox), button_quit,   TRUE, TRUE, 15);
+
+    gtk_widget_show (button_add   );
+    gtk_widget_show (button_quit  );
+    gtk_widget_show (button_delete);
+    gtk_widget_show (button_save  );
+
+    insert_highlights  (clist        );
+    gtk_widget_show (high_window );
+
+}
