c7295df9d60a425938347a66c234a6cfbba1d8f7
nobody <nobody@35a8c427-5c34-0410-aaab-be164168d6bc>
2004-07-20 21:20:27 +0000

    This commit was manufactured by cvs2svn to create branch 'GTK2'.

---
+++ branches/GTK2/ggmud/font.c
@@ -0,0 +1,135 @@
+/*  Sclient
+ *  Copyright (C) 1999 Heathen (the.heathen@swipnet.se)
+ *		  1999 Drizzt  (doc.day@swipnet.se)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#include "config.h"
+
+#include <gtk/gtk.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ggmud.h"
+
+/* Global variables */
+SYSTEM_DATA font;
+GtkWidget   *font_window;
+GtkWidget   *font_button_save;
+GtkWidget   *entry_fontname;
+GtkWidget *menu_Option_font;
+GdkFont  *font_normal;
+GtkStyle *style;
+
+void set_style() {
+    if (!font_normal)
+        return;
+
+    style = gtk_widget_get_default_style();
+
+    if (style) {
+        style->font = font_normal;
+        gtk_widget_set_default_style(style);
+
+        if (mud->window) {
+           gtk_widget_set_style(mud->window, style);
+           gtk_widget_queue_draw(mud->window);
+        }
+    }	
+}
+
+void load_font () {
+    FILE *fp;
+    gchar line[255], pref[25], value[250];
+
+#ifdef WIN32
+    font.FontName = strdup("-*-Courier New-normal-r-normal-*-*-110-*-*-m-*-iso8859-1");
+
+    // font.FontName = strdup ("-adobe-courier-medium-r-normal-*-*-120-*-*-m-*-iso8859-1");
+#else
+    font.FontName = strdup ("-*-fixed-medium-r-normal-*-*-110-*-*-c-*-iso8859-1");
+    //	font.FontName = strdup ("fixed");
+#endif
+
+    if (fp = fileopen ("font", "r")) {
+        while (fgets (line, 80, fp)) {
+            sscanf (line, "%s %[^\n]", pref, value);
+            if (!strcmp (pref, "FontName")) {
+                free(font.FontName);
+                font.FontName = strdup (value);
+            }
+        }
+
+        fclose (fp);
+    }
+
+    if ( ( font_normal = gdk_font_load (font.FontName) ) == NULL ) {
+        g_error ("Can't load font... Using default.\n");
+    }
+    else
+        set_style();
+}
+
+void save_font () {
+    FILE *fp;
+    gchar buf[256];
+
+    if(fp = fileopen("font", "w")) {
+    	if (strlen(font.FontName)) fprintf (fp, "FontName %s\n", font.FontName);
+    	fclose (fp);
+    }	
+}
+
+
+void font_font_selected (GtkWidget *button, GtkFontSelectionDialog *fs) {
+    gchar *temp, buf[256];
+
+    temp = gtk_font_selection_get_font_name (GTK_FONT_SELECTION (fs->fontsel));
+    if (temp) {
+        free (font.FontName);
+        font.FontName = strdup (temp);
+        font_normal = gdk_font_load (font.FontName);
+        set_style();
+        free (temp);
+        save_font();
+        gtk_widget_destroy (GTK_WIDGET (fs));
+    } else {
+        sprintf (buf, "The selected Font isn't valid, be sure to select a font that does exist.");
+        popup_window (buf);
+    }	    
+}
+
+void window_font (GtkWidget *button, gpointer data) {
+    GtkWidget *fontw;
+
+    fontw = gtk_font_selection_dialog_new ("Font Selection...");
+    gtk_font_selection_dialog_set_preview_text (GTK_FONT_SELECTION_DIALOG (fontw),
+                                                "This is a sample text?");
+
+    gtk_font_selection_dialog_set_font_name (GTK_FONT_SELECTION_DIALOG (fontw), font.FontName);
+
+    gtk_signal_connect (GTK_OBJECT (GTK_FONT_SELECTION_DIALOG (fontw)->ok_button), "clicked",
+                        GTK_SIGNAL_FUNC (font_font_selected), fontw);
+
+    gtk_signal_connect (GTK_OBJECT (GTK_FONT_SELECTION_DIALOG (fontw)->cancel_button), "clicked",
+                        GTK_SIGNAL_FUNC (close_window), fontw);
+
+    gtk_widget_show (fontw);
+}
+
+
---
+++ branches/GTK2/ggmud/timers.c
@@ -0,0 +1,272 @@
+#include <gtk/gtk.h>
+#include "ggmud.h"
+#include "include/ticks.h"
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+// #alias {celepath} {e;#wait 500;e;#wait 500;s;#wait 500;s;#wait 500;e;#wait 500;n;#wait 500;e;#wait 500;e;#wait 500;e;#wait 500;w;#wait 500;w;#wait 500;s;#wait 500;e;#wait 500}
+
+extern struct session *parse_input(char *, struct session *);
+extern char *get_arg_in_braces(char *s, char *arg, int flag);
+
+int use_tickcounter = 0;
+
+int timetilltick(void)
+{
+  int ttt;
+
+  ttt = (time(0) - time0) % tick_size;
+  ttt = (tick_size - ttt) % tick_size;
+  return(ttt);
+}
+
+static timerdata *get_timer_by_name(char *left)
+{
+    GList *l = mud->timers;
+
+    while (l) {
+        char *name = ((timerdata *)l->data)->string;
+
+        if (!strcmp(left, name))
+            return (timerdata *) l->data;
+        
+        l = l -> next;
+    }
+
+    return NULL;
+}
+
+
+void do_gettimer(char *arg, struct session *ses)
+{
+  extern struct listnode *common_myvars;
+  char left[BUFFER_SIZE], right[BUFFER_SIZE], arg2[BUFFER_SIZE];
+  struct listnode *tempvars, *ln;
+  timerdata *d;
+
+  tempvars = (ses ? ses->myvars : common_myvars);
+  arg = get_arg_in_braces(arg, left, 0);
+  arg = get_arg_in_braces(arg, right, 0);
+
+  if (!*left) {
+      tintin_puts("#NO VARIABLE PARAMETER INCLUDED.  TRY AGAIN.\n", ses);
+  }
+  else if(!*right) {
+      tintin_puts("#NO TIMER NAME INCLUDED. TRY AGAIN.\n", ses);
+  }
+  else if (d = get_timer_by_name(right)) {
+    time_t finish;
+    
+    if ((ln = searchnode_list(tempvars, left)))
+      deletenode_list(tempvars, ln);
+
+    finish = d->finish - time(NULL);
+    
+    sprintf(right, "%d", finish);
+    insertnode_list(tempvars, left, right, "0", ALPHA);
+    
+//    varnum++;
+    if(0) {
+      sprintf(arg2, "#Ok. $%s is now set to {%s}.", left, right);
+      tintin_puts(arg2, ses);
+    }
+  }
+  else 
+      tintin_puts("#INVALID TIMER NAME.\n", ses);
+}
+
+void do_stoptimer(char *arg, struct session *ses)
+{
+    char left[BUFFER_SIZE];
+    timerdata *d;
+    char buffer[256];
+    
+    arg = get_arg_in_braces(arg, left, 0);
+
+    if(!*left) {
+        tintin_puts("#YOU SHOULD SPECIFY A TIMER NAME\n", ses);
+        return;
+    }
+        
+    if ((d = get_timer_by_name(left))) {
+        sprintf(buffer, "#TIMER %s REMOVED.\n", left);
+
+        mud->timers = g_list_remove(mud->timers, d);
+        free(d->string);
+        free(d);
+    }
+    else 
+        sprintf(buffer, "#TIMER %s NOT FOUND.\n", left);
+
+    tintin_puts(buffer, ses);        
+}
+
+int
+put_command(char *line)
+{
+    mud->activesession = parse_input(line, mud->activesession);
+    free(line);
+
+    return FALSE;
+}
+
+void 
+wait_command(struct session *ses, char *arg, char *line)
+{
+    char temp[BUFFER_SIZE];
+    int msec;
+    
+    arg = get_arg_in_braces(arg, temp, 0);
+
+    if (!*temp) {
+        tintin_puts("#WAIT NEEDS AT LEAST A PARAMETER, LINE SKIPPED\n", ses);
+        return;
+    }
+
+    if (*arg) {
+        tintin_puts("#WAIT NEEDS ONLY ONE ARGUMENT, EXTRA ARGS SKIPPED\n", ses); 
+    }
+    
+    msec = atoi(temp);
+
+    if (msec < 100 || msec > 1000000) {
+        tintin_puts("#WAIT MUST BE BETWEEN 100 AND 1000000 MSECS\n", ses);
+        return;
+    }
+        
+    g_timeout_add(msec, (GSourceFunc)put_command, strdup(line));
+}
+
+void do_timer(char *arg, struct session *ses)
+{
+    timerdata *d;
+    int length;
+    char left[BUFFER_SIZE], right[BUFFER_SIZE];
+    
+    arg = get_arg_in_braces(arg, left, 0);
+
+    if(!*left) {
+        tintin_puts("#YOU SHOULD SPECIFY A TIMER PERIOD\n", ses);
+        return;
+    }
+    
+    arg = get_arg_in_braces(arg, right, 0);
+    
+    length = atoi(left);
+
+    if (length < 1 || length > 7200) {
+        tintin_puts("#TIMER PERIOD TOO BIG OR TOO SMALL\n", ses);
+        return;
+    }
+    
+    if (d = malloc(sizeof(timerdata))) {
+        char buffer[200];
+        
+        d->last = 0;
+        d->string = strdup(right);
+        d->finish = time(NULL) + length;
+
+        mud->timers = g_list_append(mud->timers, d);
+
+        sprintf(buffer, "#NEW TIMER %s (%d seconds)\n", d->string, length);
+        tintin_puts(buffer, ses);        
+    }
+}
+
+int checktick(void)
+{
+    extern int use_tickcounter;
+    extern int show_pretick;
+    static int last = -1, ttt = -1, using_tickcounter = 0; /* ttt = time to tick */
+    int now, found = 0;
+    GList *l = mud->timers;
+
+    now = time(0);
+
+    
+    while (l) {
+        timerdata *d = (timerdata *) l->data;
+
+        l = l->next;
+
+        if (d->last > 0 && d->last < now) {
+            time_t l = d->finish;
+
+            l -= now;
+
+            if (l == 0) {
+                char buffer[BUFFER_SIZE];
+                
+                sprintf(buffer, "#TIMER %s EXPIRED\n", d->string);
+                do_one_line(buffer, mud->activesession);
+
+                textfield_add(mud->text, buffer, MESSAGE_TICK);
+
+                free(d->string); free(d);
+                
+                mud->timers = g_list_remove(mud->timers, d);
+            }
+            else if (l == 10) {
+                char buffer[BUFFER_SIZE];
+                
+                sprintf(buffer, "#TIMER %s WILL EXPIRE IN 10 SECONDS\n", d->string);
+                do_one_line(buffer, mud->activesession);
+
+                textfield_add(mud->text, buffer, MESSAGE_TICK);
+                d->last = now;
+            }
+        }
+        else
+         d->last = now;
+    }
+    
+    if(!use_tickcounter) {
+        if(using_tickcounter) {
+            gtk_label_set_text(mud->tick_counter, "OFF");
+            using_tickcounter = 0;
+        }
+        return TRUE;
+    }
+
+    if(time0 <= 0)
+        return TRUE;	/* big number */
+ 
+    if(last > 0) {
+        while(last <= now) {
+            ttt = (++last - time0) % tick_size;
+            ttt = (tick_size - ttt) % tick_size;
+            if(!ttt || (ttt == 10 && show_pretick))
+                if(mud->activesession && mud->activesession->tickstatus)
+                {
+                   char buffer[BUFFER_SIZE];
+                   
+                   strcpy(buffer, !ttt ? "#TICK!!!\n" :
+                           "#10 SECONDS TO TICK!!!\n");
+                   
+                   do_one_line(buffer, mud->activesession);
+
+                   textfield_add(mud->text, buffer, MESSAGE_TICK);
+                }
+        }
+    } else {
+        last = now + 1;
+        ttt = (now - time0) % tick_size;
+        ttt = (tick_size - ttt) % tick_size;
+    }
+
+
+    // aggiornamento del campo coi secondi con ttt
+    if(mud->activesession && use_tickcounter) {
+        char buffer[20];
+        
+        sprintf(buffer, "%d",ttt);
+        
+        gtk_label_set_text(mud->tick_counter, buffer);
+
+        using_tickcounter = 1;
+    }
+    
+    return TRUE;
+}
+
