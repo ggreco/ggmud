e000373f34434da500523296078307dd68b77737
ggreco <ggreco@35a8c427-5c34-0410-aaab-be164168d6bc>
2006-04-26 16:53:48 +0000

    - Improved GPL header in most files. - Improved handling of the scripting triggers, now they are hidden from   the gui.

--- trunk/ggmud/window.c
+++ trunk/ggmud/window.c
@@ -1,4 +1,8 @@
-/* GGMud 
+/*  GGMud
+ *  Copyright (C) 1999 Heathen (the.heathen@swipnet.se)
+ *                1999 Drizzt  (doc.day@swipnet.se)
+ *                2003 Gabry (gabrielegreco@gmail.com)
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
