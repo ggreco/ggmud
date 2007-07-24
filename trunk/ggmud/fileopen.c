f69023ea0c21f8770ec7ef7ae1e92ba579305aa6
ggreco <ggreco@35a8c427-5c34-0410-aaab-be164168d6bc>
2007-07-24 16:33:36 +0000

    - Changed the default preferences location in win32 - Added a prefs migration routine to win32 version. - Fixed uninstall shortcut in win32 installation. - Added target to makefile to build an rpm. - Added a minimum size for the main window (600x400)

--- trunk/ggmud/fileopen.c
+++ trunk/ggmud/fileopen.c
@@ -29,9 +29,12 @@
 #include <string.h>
 #include "ggmud.h"
 
-#ifndef WIN32
+#ifdef WIN32
+#include <shlobj.h>
+#define mkdir(x, y) _mkdir(x)
+#endif
 
-int check_sclient_dir (gchar *dirname) {
+int check_ggmud_dir (gchar *dirname) {
 /* check if the specified directory exists, try to create it if it doesn't */
     struct stat file_stat;
     int return_val = 0;
@@ -43,11 +46,10 @@ int check_sclient_dir (gchar *dirname) {
             popup_window (ERR, "%s already exists and is not a directory!", dirname);
         }
     } else {
-    /* it must not exist */
-        popup_window (INFO, "%s does not exist, Creating it as a directory.", dirname);
         if (!mkdir(dirname, 0777)) {
         /* this isn't dangerous, umask modifies it */
-            popup_window (INFO, "%s created.", dirname);
+            popup_window (INFO, "GGMud settings directory <b>%s</b> created.", 
+                    dirname);
             do_manual();
         } else {
             popup_window (ERR, "%s NOT created: %s", dirname, strerror (errno));
@@ -56,23 +58,93 @@ int check_sclient_dir (gchar *dirname) {
     }
     return (return_val);
 }
-#endif
+
+#include <stdarg.h>
+
+void 
+migrate_config(const char *dest_path, ...)
+{
+    static int detected = 0;
+    char dest[255];
+    char buffer[512];
+    int len;
+    va_list va;
+    char *file;
+    FILE *fp, *out;
+    va_start(va, dest_path);
+
+    while ((file = va_arg(va, char *))) {
+
+        if ((fp = fopen(file, "rb"))) {
+            if (!detected) {
+                popup_window (INFO, "GGMud 0.7+ has changed the directory where settings are stored in Windows,\n"
+                        "the change has been made to handle different configurations for different users on the same machine.\n\n"
+                        "Since some existing settings have been found in your program directory,\n"
+                        "GGMud will NOW move them in <b>%s</b>.\n"
+                        "If you use a LUA startup script you'll have to move it by hand in the new directory.\n\n"
+                        "A copy of the original file are still present in the directory\n"
+                        "<b>disabled</b> in the program directory."
+                        , dest_path);
+
+                mkdir("disabled", 0777);
+                detected = 1;
+            }
+            g_snprintf(dest, sizeof(dest), "%s/%s", dest_path, file);
+
+            if ((out = fopen(dest, "wb"))) {
+                while ( (len = fread(buffer, 1, sizeof(buffer), fp)) > 0)
+                    fwrite(buffer, 1,  len, out);
+
+                fclose(out);
+            }
+            fclose(fp);
+
+            g_snprintf(dest, sizeof(dest), "disabled/%s", file);
+            rename(file, dest);
+        }
+    }
+    va_end(va);
+}
 
 FILE *fileopen (gchar *fname, gchar *mode) {
 /* Does all necessary checks and tries to open the specified file */
+    FILE *fp;
+    gchar path[256] = "";
+    gchar *home;
 #ifndef WIN32
     gchar *dir = "/.ggmud";
-    gchar *home;
-    gchar path[256] = "";
-    FILE *fp;
 
     home = getenv ("HOME");
-    g_snprintf (path, 255, "%s%s", home!= NULL ? home : "", dir);
-    if (check_sclient_dir(path)) return NULL;
-    g_snprintf (path, 255, "%s%s/%s", home!= NULL ? home : "", dir, fname);
+#else
+    static int check_migration = 0;
+    gchar *dir = "/ggmud";
+    TCHAR szPath[MAX_PATH];
+
+    if(SUCCEEDED(SHGetFolderPath(NULL, 
+                             CSIDL_APPDATA, 
+                             NULL, 
+                             0, 
+                             szPath))) {
+        home = szPath;
+
+        // version 0.7+ changes the location of the configuration files
+        // this routine perform a migration
+        if (!check_migration) {
+            g_snprintf(path, sizeof(path), "%s/%s", szPath, dir);
+            migrate_config(path,
+                    "triggers", "aliases", "gag", "font", "macro",
+                    "connections", "complete", "highlight",  "ggmud.prf",
+                    "Preference", "variable", "winpositions",
+                    NULL);
+            check_migration = 1;
+        }
+    }
+    else
+        home = NULL;
+#endif
+    g_snprintf (path, sizeof(path), "%s%s", home!= NULL ? home : "", dir);
+    if (check_ggmud_dir(path)) return NULL;
+    g_snprintf (path, sizeof(path), "%s%s/%s", home!= NULL ? home : "", dir, fname);
     fp = fopen (path, mode);
     return fp;
-#else
-    return fopen(fname, mode);
-#endif
 }
