f9a43a1e25ceee600da209b1d68ec30f9f96c7fc
ggreco <ggreco@35a8c427-5c34-0410-aaab-be164168d6bc>
2013-06-20 15:45:59 +0000

    Some fixes to linux installation script.

--- trunk/ggmud/setup.sh
+++ trunk/ggmud/setup.sh
@@ -2,6 +2,14 @@
 
 # sample test script to install ggmud in linux
 
-xdg-icon-resource install --size 48 --novendor ggmud.png 
-xdg-desktop-menu --novendor install ggmud.desktop 
+if [ `whoami` != "root" ]; then
+    echo "[1;6;31mYou have to execute this script as root[0m"
+    exit 1
+fi
 
+xdg-icon-resource install --size 48 --novendor ggmud.png 
+xdg-desktop-menu install --novendor ggmud.desktop 
+
+cp ggmud /usr/bin/
+cp gg_help.txt /usr/bin/
+cp leu/* /usr/bin/
